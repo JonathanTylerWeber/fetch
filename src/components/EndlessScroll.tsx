39c6015f54ef898f53561988170a495e15c6bba7
Jonathan Tyler Weber <jonathantweber@gmail.com>
2026-01-21 18:20:26 -0600

    fetch images

--- src/components/EndlessScroll.tsx
+++ src/components/EndlessScroll.tsx
@@ -1,6 +1,15 @@
 export default function EndlessScroll() {
   const url = 'https://picsum.photos/200'
 
+  async function fetchImages() {
+    try {
+      const res = await fetch(url)
+      if (!res.ok) throw new Error(`Error: ${res.status}`)
+    } catch (error) {
+      console.error(`Fetch Error: ${error}`)
+    }
+  }
+
   return (
     <div>
       <div></div>
